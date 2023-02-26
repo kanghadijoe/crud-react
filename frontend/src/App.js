582c01276824ccff8cfaff4350235b9783d17334
Hadi Pribadi <kang.hadijoe@gmail.com>
2023-02-26 15:33:24 +0700

    add page add dan edit product | mac

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import ProductList from './components/ProductList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ProductList/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/add" element={<AddProduct />} />
+          <Route path="/edit/:id" element={<EditProduct />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
